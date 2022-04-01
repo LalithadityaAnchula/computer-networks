c80c1135b7d8a1083bdcc75d5ecdc1caf967cd37
LalithadityaAnchula <lalithaditya903@gmail.com>
2022-04-01 10:19:38 +0530

    remove rarp

--- ARP_and_RARP/arp_rarp.c
+++ ARP_and_RARP/arp_rarp.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-#include <conio.h>
 #include <stdlib.h>
-void main()
+
+int main()
 {
     char ip[10][20] = {"192.168.0.64", "192.168.0.60", "192.168.0.68", "132.147.3.3"};
     char et[10][20] = {"00_A8_00_40_8E_FS", "00_16_17_31_8e_22", "00_16_17_31_8E_F7", "00_16_17_31_8E_08"};
@@ -50,4 +50,5 @@ void main()
             exit(0);
         }
     }
+    return 0;
 }
